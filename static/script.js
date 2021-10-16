5a84fa702f43858dc4b047e9e73c94e24cd2b49d
Sandipan <chattrjee.sandipan08@gmail.com>
2021-10-17 00:05:23 +0530

    Initial commit

---
+++ static/script.js
@@ -0,0 +1,7 @@
+console.log("MyTodo scripts are working");
+
+// Get the todo input Form 
+// const todoInputForm = document.getElementById("todo-input-form");
+// todoInputForm.addEventListener('submit',(e)=>{
+//     e.preventDefault()
+// })
